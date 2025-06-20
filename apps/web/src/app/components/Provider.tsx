161f498adb5c9126c137df7bf7477c181b89749f
Bianca Hua <biancahua@gmail.com>
2025-06-20 00:22:56 -0700

    chore: remove tanstack query and nextjs dev tools

--- apps/web/src/app/components/Provider.tsx
+++ apps/web/src/app/components/Provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
@@ -16,7 +16,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 } 
